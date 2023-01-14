c79c665daf95be71bfd8c9062e70e980a43ee707
rtornero1 <rtornero@chicagobooth.edu>
2023-01-14 18:37:10 +0000

    Generated Like model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :own_phothos, class_name: "Photo", foreign_key: "owner_id"
+
+  has_many :comments, foreign_key: "author_id"
+
+  has_many :likes, foreign_key: "fan_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_182323) do
+ActiveRecord::Schema.define(version: 2023_01_14_182921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2023_01_14_182323) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["photo_id"], name: "index_comments_on_photo_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "fan_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["fan_id"], name: "index_likes_on_fan_id"
+    t.index ["photo_id"], name: "index_likes_on_photo_id"
   end
 
   create_table "photos", force: :cascade do |t|
@@ -56,5 +65,7 @@ ActiveRecord::Schema.define(version: 2023_01_14_182323) do
 
   add_foreign_key "comments", "photos"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "photos"
+  add_foreign_key "likes", "users", column: "fan_id"
   add_foreign_key "photos", "users", column: "owner_id"
 end
