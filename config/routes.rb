4cc4cadec79313cb1687b2048e1b2e7ed0d005f7
rtornero1 <rtornero@chicagobooth.edu>
2023-01-17 01:19:10 +0000

    Added followers and following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root "photos#index"
 
-  
   devise_for :users
   
   resources :comments
@@ -10,6 +9,12 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "photos#liked", as: :liked_photos
+
+  get ":username/feed" => "photos#following", as: :photos_following
+
+  get ":username/followers" => "users#followers", as: :followers_users
+  
+  get ":username/following" => "users#following", as: :following_users
 
   get ":username" => "users#show", as: :user
 end
